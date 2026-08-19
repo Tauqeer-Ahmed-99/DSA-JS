1b37c26777f3db440f913e9f22de8512dcb4d461
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2026-08-19 20:41:21 +0530

    DLL code tasks

--- src/doubly-linked-list/doubly-linked-list.ts
+++ src/doubly-linked-list/doubly-linked-list.ts
@@ -208,4 +208,181 @@ export default class DoublyLinkedList<T> {
 
     return temp;
   }
+
+  isPalindrome(): boolean {
+    if (this.length === 0 || this.length === 1) {
+      return true;
+    }
+
+    let forward = this.head;
+    let backward = this.tail;
+
+    const iterations = this.length / 2;
+
+    for (let i = 0; i < iterations; i++) {
+      if (forward?.value !== backward?.value) {
+        return false;
+      }
+
+      if (forward) forward = forward.next;
+      if (backward) backward = backward.prev;
+    }
+
+    return true;
+  }
+
+  reverse(): DoublyLinkedList<T> {
+    let current = this.head;
+
+    while (current) {
+      if (current) {
+        const temp = current.prev;
+
+        current.prev = current.next;
+        current.next = temp;
+
+        current = current.prev;
+      }
+    }
+
+    const temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+
+    return this;
+  }
+
+  partitionList(x: number): DoublyLinkedList<T> {
+    let lessHead: Node<T> | null = null;
+    let lessTail: Node<T> | null = null;
+    let greaterHead: Node<T> | null = null;
+    let greaterTail: Node<T> | null = null;
+
+    let current = this.head;
+
+    while (current) {
+      const next = current.next;
+
+      current.next = null;
+
+      if ((current.value as number) < x) {
+        if (lessHead && lessTail) {
+          lessTail.next = current;
+          current.prev = lessTail;
+          lessTail = current;
+        } else {
+          lessHead = current;
+          lessTail = current;
+        }
+      } else {
+        if (greaterHead && greaterTail) {
+          greaterTail.next = current;
+          current.prev = greaterTail;
+          greaterTail = current;
+        } else {
+          greaterHead = current;
+          greaterTail = current;
+        }
+      }
+
+      current = next;
+    }
+
+    // These Edge cases are important
+
+    if (lessTail && greaterHead) {
+      lessTail.next = greaterHead;
+      greaterHead.prev = lessTail;
+    }
+
+    this.head = lessHead ? lessHead : greaterHead;
+    this.tail = greaterTail ? greaterTail : lessTail;
+
+    return this;
+  }
+
+  reverseBetween(start: number, end: number): DoublyLinkedList<T> {
+    if (!this.head || start < 0 || end >= this.length || start >= end) {
+      return this;
+    }
+
+    let prev: Node<T> | null = null;
+    let current: Node<T> | null = this.head;
+
+    for (let i = 0; i < start; i++) {
+      prev = current;
+      if (current) current = current.next;
+    }
+
+    const before = prev;
+    const sectionStart = current;
+
+    for (let i = start; i <= end; i++) {
+      const next = current?.next ?? null;
+      if (current) {
+        current.next = current.prev;
+        current.prev = next;
+      }
+      prev = current;
+      current = next;
+    }
+
+    if (before) {
+      before.next = prev;
+      if (prev) prev.prev = before;
+    } else {
+      this.head = prev;
+    }
+
+    if (sectionStart) {
+      sectionStart.next = current;
+      if (current) current.prev = sectionStart;
+    }
+
+    if (end === this.length - 1) {
+      this.tail = sectionStart;
+    }
+
+    return this;
+  }
+
+  swapPairs(): DoublyLinkedList<T> {
+    if (!this.head || !this.head.next) {
+      return this;
+    }
+
+    let prev: Node<T> | null = null;
+    let current: Node<T> | null = this.head;
+
+    // need pairs therefore both current && current.next
+    while (current && current.next) {
+      const first: Node<T> | null = current;
+      const second: Node<T> | null = first.next;
+
+      // Swap
+      first.next = second?.next ?? null;
+      first.prev = second;
+      if (second) {
+        second.next = first;
+        second.prev = prev;
+      }
+
+      //Connect Prev to new pair first
+
+      if (prev) {
+        prev.next = second;
+      } else {
+        this.head = second;
+      }
+
+      // prev becomes the last node of the pair
+      prev = first;
+
+      current = first.next;
+    }
+
+    this.tail = prev;
+
+    return this;
+  }
 }
